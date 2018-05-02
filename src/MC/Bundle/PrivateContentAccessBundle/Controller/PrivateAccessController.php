f7e5d89523f1e60c93b7e26a10ccef2c479ee1e0
mbouhlel75 <m.bouhlel@novactive.com>
2018-05-02 07:31:10 +0200

    code-clean

--- src/MC/Bundle/PrivateContentAccessBundle/Controller/PrivateAccessController.php
+++ src/MC/Bundle/PrivateContentAccessBundle/Controller/PrivateAccessController.php
@@ -14,43 +14,23 @@ class PrivateAccessController extends Controller
 {
     public function privateAccessAction(Request $request)
     {
-
-        // 1) build the form
         $privateAccessForm = new PrivateAccess();
         $form = $this->createForm(PrivateAccessForm::class, $privateAccessForm);
 
-        // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($request->isMethod('POST')) {
-//$form->isSubmitted() && $form->isValid() &&
-            //var_dump($form);die();
-            // 3) Encode the password (you could also do this via Doctrine listener)
+
             //$password = $passwordEncoder->encodePassword($privateAccessForm, $privateAccessForm->getPlainPassword());
 
-            //$privateAccessForm->setPassword($password);
-//var_dump($form->getData()->getPlainPassword());die();
-            $privateAccessForm->setLocationPath('1/2/4/384');
-            $privateAccessForm->setPassword($form->getData()->getPlainPassword());
-$privateAccessForm->setCreated((new \DateTime()));
+            $privateAccessForm->setCreated((new \DateTime()));
 
-            // 4) save the User!
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($privateAccessForm);
             $entityManager->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
-
             return $this->templating->renderResponse($template, ['form' => $form->createView()], new Response());
-
-            //return $this->redirectToRoute('replace_with_some_route');
         }
 
-        //return $this->templating->renderResponse($template, ['form' => $form->createView()], new Response());
-
-        //var_dump($form);die();
-
-        //return new ContentView("@MCPrivateAccessBundle::tabs/private_content_tab_form.html.twig" , ['form' => $form]);
         return $this->render(
             '@MCPrivateContentAccess/tabs/private_content_tab_form.html.twig',
             array('form' => $form->createView())
--- src/MC/Bundle/PrivateContentAccessBundle/Custom/PrivateContentTab.php
+++ src/MC/Bundle/PrivateContentAccessBundle/Custom/PrivateContentTab.php
@@ -27,14 +27,8 @@ class PrivateContentTab extends AbstractTab implements OrderedTabInterface
     /** @var SearchService */
     protected $searchService;
 
-    /** @var Request */
-    //protected $request;
-
     /** @var FormFactory */
     protected $formFactory;
-
-    /** @var service_container */
-    //protected $container;
 
     public function __construct(
         Environment $twig,
@@ -42,21 +36,19 @@ class PrivateContentTab extends AbstractTab implements OrderedTabInterface
         PagerContentToDataMapper $pagerContentToDataMapper,
         SearchService $searchService,
         FormFactory $formFactory
-        //Container $container
 
     ) {
         parent::__construct($twig, $translator);
 
         $this->pagerContentToDataMapper = $pagerContentToDataMapper;
         $this->searchService = $searchService;
-        //$this->request = $request;
         $this->formFactory = $formFactory;
 
     }
 
     public function getIdentifier(): string
     {
-        return 'make-private';
+        return 'private-access';
     }
 
     public function getName(): string
@@ -71,66 +63,7 @@ class PrivateContentTab extends AbstractTab implements OrderedTabInterface
 
     public function renderView(array $parameters): string
     {
-        //var_dump($parameters['content']);die();
-
-        //$page = 1;
-        //$limit = 10;
-
-        //$query = new LocationQuery();
-
-        //$query->sortClauses = [new SortClause\DateModified(LocationQuery::SORT_DESC)];
-        //$query->query = new Criterion\LogicalAnd([
-        //    new Criterion\ContentTypeIdentifier('article'),
-        //]);
-
-        /*$pager = new Pagerfanta(
-            new ContentSearchAdapter($query,
-                $this->searchService
-            )
-        );*/
-        /*$pager->setMaxPerPage($limit);
-        $pager->setCurrentPage($page);
-
-        return $this->twig->render('PrivateContentBundle:tabs:private_content_tab.html.twig', [
-            'data' => $this->pagerContentToDataMapper->map($pager),
-        ]);*/
-
-
-
-        // 1) build the form
         $privateAccess = new PrivateAccess();
-
-
-        // $form = $this->container()->get("form.factory");
-//        $form = $this->createForm(PrivateAccessForm::class, $privateAccess);
-
-        // 2) handle the submit (will only happen on POST)
-        /*$form->handleRequest($this->request);
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->encodePassword($makePrivate, $makePrivate->getPlainPassword());
-            $makePrivate->setPassword($password);
-
-            // 4) save the User!
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($makePrivate);
-            $entityManager->flush();
-
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
-
-            return $this->redirectToRoute('replace_with_some_route');
-        }*/
-/*
-        return $this->render(
-            'registration/register.html.twig',
-            array('form' => $form->createView())
-        );*/
-
-
-
-
 
         return $this->twig->render('@MCPrivateContentAccess/tabs/private_content_tab.html.twig', [
             'name' => $this->getName()
