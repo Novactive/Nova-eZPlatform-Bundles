e94145eedec2451a95f1f25b9e0b4d5ac8613653
Florian ALEXANDRE <f.alexandre@novactive.com>
2018-07-19 16:49:15 +0200

    feat: http cache invalidation

---
+++ src/lib/EventListener/CachePurgerListener.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @copyright Novactive
+ * Date: 19/07/18
+ */
+
+namespace Novactive\EzMenuManager\EventListener;
+
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use FOS\HttpCache\Handler\TagHandler;
+use Novactive\EzMenuManagerBundle\Entity\Menu;
+
+class CachePurgerListener
+{
+    /** @var TagHandler */
+    protected $tagHandler;
+
+    /**
+     * CachePurgerListener constructor.
+     * @param TagHandler $tagHandler
+     */
+    public function __construct( TagHandler $tagHandler )
+    {
+        $this->tagHandler = $tagHandler;
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist( LifecycleEventArgs $args )
+    {
+        $this->purgeMenuCache( $args );
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postUpdate( LifecycleEventArgs $args )
+    {
+        $this->purgeMenuCache( $args );
+
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postRemove( LifecycleEventArgs $args )
+    {
+        $this->purgeMenuCache( $args );
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function purgeMenuCache( LifecycleEventArgs $args )
+    {
+        $entity = $args->getEntity();
+
+        if ( !$entity instanceof Menu )
+        {
+            return;
+        }
+
+        $this->tagHandler->invalidateTags( [ 'menu-' . $entity->getId() ] );
+    }
+}
