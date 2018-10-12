52a0a2aad816f42b8ef231ebe9dcd343736485ad
Florian ALEXANDRE <f.alexandre@novactive.com>
2018-10-12 12:32:16 +0200

    feat: index binary file text value for full text search

---
+++ src/bundle/DependencyInjection/EzSolrSearchExtrasExtension.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * NovaeZSolrSearchExtras.
+ *
+ * @package   NovaeZSolrSearchExtras
+ *
+ * @author    Novactive <f.alexandre@novactive.com>
+ * @copyright 2018 Novactive
+ * @license   https://github.com/Novactive/NovaeZSolrSearchExtras/blob/master/LICENSE
+ */
+
+namespace Novactive\EzSolrSearchExtrasBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class EzSolrSearchExtrasExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
