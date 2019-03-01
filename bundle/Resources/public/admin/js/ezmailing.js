e6e78fcd3d245b573138474b7ac3a5c489186d4b
Maxim Strukov <mxmstrv@gmail.com>
2019-02-28 18:15:04 -0800

    fixed js to expand activce on page load

--- bundle/Resources/public/admin/js/ezmailing.js
+++ bundle/Resources/public/admin/js/ezmailing.js
@@ -15,7 +15,7 @@ jQuery(function () {
         window.location = $(this).parent().find('ul > li.subscriptions > a').attr('href');
         return false;
     });
-
+    $('.campaigns > ul > li.current_ancestor').addClass('expand');
     $('.campaigns > ul > li > input').click(function () {
         if ($(this).prop('checked')) {
             $(this).parent().addClass('expand');
