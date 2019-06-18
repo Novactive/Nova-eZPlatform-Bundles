08ad854275a8412935dab04c90057a8cfd95e0b4
mike <serejamike@gmail.com>
2019-06-18 11:30:35 +0300

    Check is event preContentView instance of ContentEditView

--- bundle/Listener/PreContentView.php
+++ bundle/Listener/PreContentView.php
@@ -5,6 +5,7 @@ namespace Novactive\Bundle\NovaeZEditHelpBundle\Listener;
 
 use Novactive\Bundle\NovaeZEditHelpBundle\Services\FetchDocumentation;
 use eZ\Publish\Core\MVC\Symfony\Event\PreContentViewEvent;
+use EzSystems\RepositoryForms\Content\View\ContentEditView;
 
 class PreContentView
 {
@@ -17,25 +18,26 @@ class PreContentView
 
     public function onPreContentView(PreContentViewEvent $event)
     {
-        $contentType = $event->getContentView()->getContent()->getContentType();
-        $documentation = $this->fetchDocumentation->getByContentType($contentType);
+        if ($event->getContentView() instanceof ContentEditView) {
+            $contentType = $event->getContentView()->getContent()->getContentType();
+            $documentation = $this->fetchDocumentation->getByContentType($contentType);
 
-        if (!empty($documentation)) {
-            $mainLocationId = $documentation->valueObject->contentInfo->mainLocationId;
-            $children = $this->fetchDocumentation->getChildrenByLocationId($mainLocationId);
+            if (!empty($documentation)) {
+                $mainLocationId = $documentation->valueObject->contentInfo->mainLocationId;
+                $children = $this->fetchDocumentation->getChildrenByLocationId($mainLocationId);
 
 
-            $items = [];
-            if (!empty($children)) {
-                foreach ($children as $child) {
-                    $identifier = (string) $child->valueObject->getFieldValue('identifier');
-                    $items[$identifier] = $child->valueObject;
+                $items = [];
+                if (!empty($children)) {
+                    foreach ($children as $child) {
+                        $identifier = (string) $child->valueObject->getFieldValue('identifier');
+                        $items[$identifier] = $child->valueObject;
+                    }
                 }
+
+                $event->getContentView()->addParameters(['items' => $items]);
             }
-
-            $event->getContentView()->addParameters(['items' => $items]);
         }
-
     }
 
 }
