e2f651d329639eb3b58299f78e9c674b19b8f4e0
Ousmane KANTE <o.kante@novactive.com>
2019-12-04 13:55:45 +0100

    using '@required' instead of calls

--- src/bundle/Controller/EntityManagerTrait.php
+++ src/bundle/Controller/EntityManagerTrait.php
@@ -9,6 +9,10 @@ trait EntityManagerTrait
     /** @var EntityManagerInterface */
     public $entityManager;
 
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @required
+     */
     public function setEntityManager(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
