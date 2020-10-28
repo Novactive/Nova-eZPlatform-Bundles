ae8c3a6bca801b37f4417ca95b14b22fd0a65888
Ousmane KANTE <o.kante@novactive.com>
2020-10-28 03:45:20 +0100

    Maintenance system: remove dates from FormType (to be manage later)

--- components/MaintenanceBundle/bundle/Form/Type/FilterType.php
+++ components/MaintenanceBundle/bundle/Form/Type/FilterType.php
@@ -16,37 +16,13 @@ namespace Novactive\NovaeZMaintenanceBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class FilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $classes = 'flatpickr flatpickr-input ez-data-source__input form-control';
         $builder
-            ->add(
-                'min_created',
-                TextType::class,
-                [
-                    'label' => 'Starts',
-                    'required' => true,
-                    'attr' => [
-                        'class' => $classes.' date-start',
-                    ],
-                ]
-            )
-            ->add(
-                'max_created',
-                TextType::class,
-                [
-                    'label' => 'Ends',
-                    'required' => true,
-                    'attr' => [
-                        'class' => $classes.' date-end',
-                    ],
-                ]
-            )
             ->add(
                 'submit',
                 SubmitType::class,
