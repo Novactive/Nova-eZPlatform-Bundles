0fafd61eb03f7647b20726e6953531fda102e5c5
clotildeh <c.hebert@novactive.com>
2023-02-27 11:41:24 +0100

    seo bundle => fix JS import URL

--- components/SEOBundle/bundle/Resources/public/js/script.js
+++ components/SEOBundle/bundle/Resources/public/js/script.js
@@ -1,12 +1,16 @@
 const uploadBtn = document.getElementsByClassName('novaseo-upload-btn')[0];
 const uploadInput = document.getElementsByClassName('novaseo-upload-file')[0];
-uploadBtn.addEventListener('click', function() {
-    uploadInput.click();
-});
+if (uploadBtn) {
+    uploadBtn.addEventListener('click', function () {
+        uploadInput.click();
+    });
+}
 
-uploadInput.addEventListener('change', function(e) {
-    var fileName = e.target.files[0].name;
-    const uploadPreview = document.getElementsByClassName('novaseo-upload-preview')[0];
-    uploadPreview.style.display = 'flex';
-    uploadPreview.innerHTML = fileName;
-});
+if (uploadInput) {
+    uploadInput.addEventListener('change', function (e) {
+        var fileName = e.target.files[0].name;
+        const uploadPreview = document.getElementsByClassName('novaseo-upload-preview')[0];
+        uploadPreview.style.display = 'flex';
+        uploadPreview.innerHTML = fileName;
+    });
+}
