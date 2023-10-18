c844c8519c72eb53f79b847ec53cf2cfadec3485
Florian ALEXANDRE <f.alexandre@novactive.com>
2023-10-18 08:35:37 +0200

    feat: AlmaviaCXIbexaRichTextExtraBundle initialization

---
+++ components/RichTextExtraBundle/src/bundle/Resources/encore/ibexa.richtext.config.manager.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = (ibexaConfig, ibexaConfigManager) => {
+  ibexaConfigManager.add({
+    ibexaConfig,
+    entryName: 'ibexa-richtext-onlineeditor-js',
+    newItems: [
+      path.resolve(__dirname, '../public/js/CKEditor/embed/extraconfig.js')
+    ]
+  })
+}
