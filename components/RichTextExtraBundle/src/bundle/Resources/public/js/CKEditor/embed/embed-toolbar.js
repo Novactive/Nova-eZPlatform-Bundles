c9cd24ec7a6159eca517a9824725789d4f9dd30b
Florian ALEXANDRE <f.alexandre@novactive.com>
2023-11-17 15:16:09 +0100

    feat: add embed toolbar plugin

---
+++ components/RichTextExtraBundle/src/bundle/Resources/public/js/CKEditor/embed/embed-toolbar.js
@@ -0,0 +1,28 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import WidgetToolbarRepository from '@ckeditor/ckeditor5-widget/src/widgettoolbarrepository';
+
+class IbexaEmbedToolbar extends Plugin {
+    static get requires() {
+        return [WidgetToolbarRepository];
+    }
+
+    getSelectedEmbedWidget(selection) {
+        const viewElement = selection.getSelectedElement();
+        const isEmbed = viewElement?.hasClass('ibexa-embed');
+
+        return isEmbed ? viewElement : null;
+    }
+
+    afterInit() {
+        const { editor } = this;
+        const widgetToolbarRepository = editor.plugins.get(WidgetToolbarRepository);
+
+        widgetToolbarRepository.register('embed', {
+            ariaLabel: editor.t('Embed toolbar'),
+            items: editor.config.get('embed.toolbar') || [],
+            getRelatedElement: this.getSelectedEmbedWidget,
+        });
+    }
+}
+
+export default IbexaEmbedToolbar;
