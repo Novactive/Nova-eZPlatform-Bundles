09fc224dbe97e60a8c63155b687a8f9087d5e9e2
Florian ALEXANDRE <f.alexandre@novactive.com>
2024-02-29 11:20:18 +0100

    feat: saml bundle

--- components/SamlBundle/src/lib/Security/Saml/SamlUserProvider.php
+++ components/SamlBundle/src/lib/Security/Saml/SamlUserProvider.php
@@ -4,47 +4,40 @@ declare(strict_types=1);
 
 namespace AlmaviaCX\Bundle\IbexaSaml\Security\Saml;
 
-use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
 use Ibexa\Contracts\Core\Repository\PermissionResolver;
-use Ibexa\Contracts\Core\Repository\UserService;
-use Ibexa\Core\MVC\Symfony\Security\UserInterface;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Ibexa\Core\MVC\Symfony\Security\User\BaseProvider;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class SamlUserProvider implements UserProviderInterface
 {
-    protected SamlExceptionLogger $ssoExceptionLogger;
+    protected BaseProvider $baseProvider;
+    protected PermissionResolver $permissionResolver;
 
-    protected UserProviderInterface $baseProvider;
-
-    /**
-     * @param \AlmaviaCX\Bundle\IbexaSaml\Security\Saml\SamlExceptionLogger $ssoExceptionLogger
-     * @param \Symfony\Component\Security\Core\User\UserProviderInterface $baseProvider
-     */
-    public function __construct( SamlExceptionLogger $ssoExceptionLogger, UserProviderInterface $baseProvider )
+    public function __construct(BaseProvider $baseProvider, PermissionResolver $permissionResolver)
     {
-        $this->ssoExceptionLogger = $ssoExceptionLogger;
         $this->baseProvider = $baseProvider;
+        $this->permissionResolver = $permissionResolver;
     }
 
-    public function refreshUser( \Symfony\Component\Security\Core\User\UserInterface $user )
+    public function refreshUser(\Symfony\Component\Security\Core\User\UserInterface $user)
     {
         return $this->baseProvider->refreshUser($user);
     }
 
-    public function supportsClass( string $class )
+    public function supportsClass(string $class)
     {
         return $this->baseProvider->supportsClass($class);
     }
 
-    public function loadUserByIdentifier($identifier): UserInterface
+    public function loadUserByIdentifier(string $identifier)
     {
-        return $this->baseProvider->loadUserByUsername($identifier);
+        $user = $this->baseProvider->loadUserByUsername($identifier);
+        $this->permissionResolver->setCurrentUserReference($user->getAPIUser());
+
+        return $user;
     }
 
-    public function loadUserByUsername( string $username )
+    public function loadUserByUsername(string $username)
     {
         return $this->baseProvider->loadUserByUsername($username);
     }
