e7765333bdd76de077bef4ed96e8fa9bac3eef68
Florian ALEXANDRE <f.alexandre@novactive.com>
2024-04-25 11:32:00 +0200

    feat: file d&d into richtext

--- components/RichTextExtraBundle/src/bundle/Resources/public/js/CKEditor/upload-file/upload-file-editing.js
+++ components/RichTextExtraBundle/src/bundle/Resources/public/js/CKEditor/upload-file/upload-file-editing.js
@@ -8,6 +8,36 @@ class UploadFileEditing extends Plugin {
     }
 
     addListeners() {
+        this.listenTo(this.editor.editing.view.document, 'drop', (event, data) => {
+            if (data.dataTransfer.effectAllowed === 'copyMove') {
+                return;
+            }
+
+            const { files } = data.dataTransfer;
+
+            if (!files.length) {
+                return;
+            }
+
+            this.editor.model.change((writer) => {
+                writer.setSelection(this.editor.editing.mapper.toModelRange(data.dropRange));
+            });
+
+            const allowedMimeTypes = ibexa.adminUiConfig.fileUpload.mime_types || []
+            const readFile = function(file, resolve, reject) {
+                this.addEventListener('load', () => resolve({fileReader: this, file}), false);
+                this.addEventListener('error', () => reject(), false);
+                this.readAsDataURL(file);
+            }
+
+            files.forEach((file) => {
+                if (allowedMimeTypes.includes(file.type)) {
+                    new Promise(readFile.bind(new FileReader(), file)).then(({file}) => {
+                        this.editor.execute('insertIbexaUploadFile', { file });
+                    })
+                }
+            });
+        });
     }
 
     init() {
