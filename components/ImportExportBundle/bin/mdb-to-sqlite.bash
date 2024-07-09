fc127e76347e8d4cfc6c10c9ee6978637f34b0eb
Florian ALEXANDRE <f.alexandre@novactive.com>
2024-07-09 08:47:17 +0200

    feat: import export bundle

--- components/ImportExportBundle/bin/mdb-to-sqlite.bash
+++ components/ImportExportBundle/bin/mdb-to-sqlite.bash
@@ -11,7 +11,7 @@ echo "Starting conversion in ${tmp_dir}"
 mkdir -p $tmp_dir
 mdb-schema $mdb_path sqlite > ${tmp_dir}/schema.sql
 mkdir -p ${tmp_dir}/sql
-for i in $( mdb-tables $mdb_path ); do mdb-export -D "%Y-%m-%d %H:%M:%S" -H -I sqlite $mdb_path $i > ${tmp_dir}/sql/$i.sql; done
+for i in $( mdb-tables $mdb_path ); do mdb-export --quote="'" -D "%Y-%m-%d %H:%M:%S" -H -I sqlite $mdb_path $i > ${tmp_dir}/sql/$i.sql; done
 cat ${tmp_dir}/schema.sql | sqlite3 $sqlite_path
 for f in ${tmp_dir}/sql/* ; do (echo 'BEGIN;'; cat $f; echo 'COMMIT;') | sqlite3 $sqlite_path; done
 rm -rf $tmp_dir
