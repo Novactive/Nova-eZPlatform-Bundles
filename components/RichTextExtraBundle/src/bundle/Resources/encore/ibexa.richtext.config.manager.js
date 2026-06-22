638fc139873f1c6fee108ed9970650fea8c4f110
Rémy LIMOUZIN <remy.limouzin@almaviacx.com>
2026-06-22 17:41:31 +0200

    almaviacx/ibexarichtextextrabundle IBEXA 5 Remove useless features and fix the remaining features

--- components/RichTextExtraBundle/src/bundle/Resources/encore/ibexa.richtext.config.manager.js
+++ components/RichTextExtraBundle/src/bundle/Resources/encore/ibexa.richtext.config.manager.js
@@ -1,17 +1,16 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = (ibexaConfig, ibexaConfigManager) => {
   ibexaConfigManager.add({
     ibexaConfig,
     entryName: 'ibexa-richtext-onlineeditor-js',
     newItems: [
-      path.resolve(__dirname, '../public/js/CKEditor/extraconfig.js')
-    ]
-  })
+      path.resolve(__dirname, '../public/js/CKEditor/extraconfig.js'),
+    ],
+  });
 
   Object.assign(ibexaConfig.resolve.alias, {
     '@ibexa-richtext': path.resolve('./vendor/ibexa/fieldtype-richtext'),
-    '@ibexa-admin-ui': path.resolve('./vendor/ibexa/admin-ui')
+    '@ibexa-admin-ui': path.resolve('./vendor/ibexa/admin-ui'),
   });
-
-}
+};
--- components/RichTextExtraBundle/src/bundle/Resources/public/js/CKEditor/extraconfig.js
+++ components/RichTextExtraBundle/src/bundle/Resources/public/js/CKEditor/extraconfig.js
@@ -1,34 +1,7 @@
-import EmbedToolbar from "./embed/embed-toolbar";
-import EmbedImageEdit from "./embed-edit/image/embed-image-edit";
-import EmbedContentEdit from "./embed-edit/content/embed-content-edit";
-import UploadFile from "./upload-file/upload-file";
+import UploadFile from './upload-file/upload-file';
 
 (function (ibexa) {
-
     ibexa.addConfig('richText.CKEditor.extraPlugins', [
-        EmbedImageEdit,
-        EmbedContentEdit,
-        EmbedToolbar,
-        UploadFile
+        UploadFile,
     ], true);
-
-    ibexa.addConfig('richText.CKEditor.extraConfig', {
-        embedImage: {
-            toolbar: [
-                'imageVarations',
-                'ibexaBlockLeftAlignment',
-                'ibexaBlockCenterAlignment',
-                'ibexaBlockRightAlignment',
-                'ibexaRemoveElement',
-                'embedImageEdit',
-            ],
-        },
-        embed: {
-            toolbar: [
-                'embedContentEdit',
-            ],
-        }
-    }, true);
-
-
 })(window.ibexa);
